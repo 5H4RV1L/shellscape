7eb85722011d5e4fcfb3944021a1069f2d30d673
5H4RV1L <156880539+5H4RV1L@users.noreply.github.com>
2026-04-03 23:01:49 +0530

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,1 +1,15 @@
+function executeCommand(input) {
+    const [cmd, ...args] = input.trim().split(" ");
+    const arg = args.join(" ");
 
+    if (COMMANDS[cmd]) {
+        return COMMANDS[cmd](LEVEL, arg);
+    }
+
+    // level-specific commands
+    if (LEVEL.expectedCommands[input]) {
+        return LEVEL.expectedCommands[input];
+    }
+
+    return "command not found";
+}
